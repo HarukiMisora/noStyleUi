f7849db6ef4ec956aad9cb9adfdbb539c8e89f71
lilmonix3 <wwssjjhhaamm@qq.com>
2025-03-19 21:10:42 +0800

    fix:什么都没改...

--- noStyleUi/div/Div.ts
+++ noStyleUi/div/Div.ts
@@ -36,6 +36,7 @@ const flexOptionActive = {
 interface renderHelperOptionsT {
     disabled?:boolean
 }
+//颜色混合
 const setMixColor =(colors:string[])=>{
     if(!isValidColor(colors[0])&&!colorValues.includes(colors[0])){
         console.error(colors[0],'不是一个合法颜色')
@@ -59,6 +60,16 @@ const setMixColor =(colors:string[])=>{
         return l-e>1?mixColor:lastColor
     }
 }
+//遍历属性集合
+const mapProps =(values:string[],call:(value:string)=>void) =>{
+    let l = values.length
+    for(let i=0;i<l;i++){
+        call(values[i])
+    }
+    
+}
+
+
 export function renderHelper(props:PropT,options:renderHelperOptionsT){
 
     const className:{[key:string]:Boolean} ={}
@@ -129,157 +140,115 @@ export function renderHelper(props:PropT,options:renderHelperOptionsT){
     }
 
     const bgColors:string[] = [];
-    const setBgOptions = (arr:any[])=>{
+    
+    if(props.bg!==undefined){
         const size =['auto','auto']
-            for(let i of arr){
+        mapProps(Array.isArray(props.bg)?props.bg:props.bg.split(' '),(value:string)=>{
+            if(isImage(value)){
+                if(!value){
+                    return
+                }
+                styles.backgroundImage = `url("${value}")`
+                return
+            }
+            if(['fill','contain','cover','cover','none'].includes(value)){
+                className[`bs-${value}`] = true
+                return
+            }
+            if('center' === value){
+                className['bp-center'] = true
+                return
+            }
+            if(new RegExp('^.*(left|bottom|top|right|center)$').test(value)){
+                const options = value.split('-');
+                for(let d of options){
+                    // console.log(d);
+                    switch(d){
+                        case 'left':className['bp-x-left']=true;break;
+                        case 'right':className['bp-x-right']=true;break;
+                        case 'top':className['bp-y-top']=true;break;
+                        case 'bottom':className['bp-y-bottom']=true;break;
+                        case 'center':className['bp-center']=true;break;
+                        default:console.warn(d,'不是一个合法的option')
+                    }
+                    
+                }
+                return
+            }
+
+            if(value.slice(0,1)==='#'){
+                // console.log(i);
+                bgColors.push(value)
+                // console.log(bgColors[0]);
+                
+                if(bgColors.length>1){
+                    const lastColor = styles.backgroundColor?styles.backgroundColor:bgColors[0]
+                    className[`bc-${bgColors[0]}`] = false
+                    className[`bc-${value}`] = false
+                    styles.backgroundColor = `color-mix(in lch, ${lastColor}, ${value})`
+                }else{
+                    styles.backgroundColor = value
+                }
+
+                return
+            }
+            if(value.indexOf('-') === -1){
+                className[`bc-${value}`] = true
+                bgColors.push(value)
+                delete styles.background
+
+                if(bgColors.length>1){
+                    className[`bc-${bgColors[0]}`] = false
+                    className[`bc-${value}`] = false
+                    
+                    const lastColor = styles.backgroundColor?styles.backgroundColor:bgColors[0]
+                    
+                    styles.backgroundColor = `color-mix(in lch, ${lastColor}, ${value})`
+                }
                 // console.log(i);
                 
                 
-                if(!i){
-                    continue
-                }
-                if(isImage(i)){
-                    // console.log(props.bg);
-                    styles.backgroundImage = `url("${i}")`
-                    continue
-                }
-                if(['fill','contain','cover','cover','none'].includes(i)){
-                    
-                    className[`bs-${i}`] = true
-                    continue
-                }
-                if('center' === i){
-                    className['bp-center'] = true
-                    continue
-                }
-
-                if(new RegExp('^.*(left|bottom|top|right|center)$').test(i)){
-                    const options = i.split('-');
-                    for(let d of options){
-                        // console.log(d);
-                        switch(d){
-                            case 'left':className['bp-x-left']=true;break;
-                            case 'right':className['bp-x-right']=true;break;
-                            case 'top':className['bp-y-top']=true;break;
-                            case 'bottom':className['bp-y-bottom']=true;break;
-                            case 'center':className['bp-center']=true;break;
-                        }
-                        
-                    }
-                    continue
-                }
-                // console.log(i.slice(0,1));
-            
-                 
-
-                if(i.slice(0,1)==='#'){
-                    // console.log(i);
-                    bgColors.push(i)
-                    // console.log(bgColors[0]);
-                    
-                    if(bgColors.length>1){
-                        const lastColor = styles.backgroundColor?styles.backgroundColor:bgColors[0]
-                        className[`bc-${bgColors[0]}`] = false
-                        className[`bc-${i}`] = false
-                        styles.backgroundColor = `color-mix(in lch, ${lastColor}, ${i})`
-                    }else{
-                        styles.backgroundColor = i
-                    }
-
-                    continue
-                }
-                if(i.indexOf('-') === -1){
-                    className[`bc-${i}`] = true
-                    bgColors.push(i)
-                    delete styles.background
-
-                    if(bgColors.length>1){
-                        className[`bc-${bgColors[0]}`] = false
-                        className[`bc-${i}`] = false
-                        
-                        const lastColor = styles.backgroundColor?styles.backgroundColor:bgColors[0]
-                        
-                        styles.backgroundColor = `color-mix(in lch, ${lastColor}, ${i})`
-                    }
-                    // console.log(i);
-                    
-                    continue
-                }
-                
-                
-         
-
-
-                
-                const option = i.split('-')
-                // console.log(option);
-                let thisClass = ''
-                const sizePix = option[1]?.indexOf('p')?(option[1]?.indexOf('v')?'px':`v${option[0]}`):'%'
-                const sizeValue = sizePix === 'px'?option[1]:option[1]?.slice(1)
-
-                switch(option[0]){
-                    case 'w':size[0] = sizeValue + sizePix;break;
-                    case 'h':size[1] = sizeValue + sizePix;break;
-                    case 'c':className[`bc-${option?.[1]}`] = true;break;
-                    case 's':className[`bs-${option?.[1]}`] = true;break;
-                    case 'x':className[`bp-x-${option?.[1]}`] = true;break;
-                    case 'y':className[`bp-y-${option?.[1]}`] = true;break;
-                    case 'r':className[`bp-r-${option?.[1]}`] = true;break;
-                    default:{
-                        for(let i of option){
-                            bgColors.push(i)
-                            if(bgColors.length>1){
-                                className[`bc-${bgColors[0]}`] = false
-                                className[`bc-${i}`] = false
-                                
-                                const lastColor = styles.backgroundColor?styles.backgroundColor:bgColors[0]
-                                
-                                styles.backgroundColor = `color-mix(in lch, ${lastColor}, ${i})`
-                            }
-                        }
-
-                        // thisClass = bgOptionActive[<keyof typeof bgOptionActive>option?.[0]]?.(option?.[1])
-                    }break;
-                }
-                if(thisClass){
-                    className[thisClass] = true
-                }
-
-                // console.log(size,styles);
-                if(size[0]!=='auto'||size[1]!=='auto'){
-                    styles.backgroundSize = `${size[0]} ${size[1]}`
-                }
-                
-
-                // console.log(styles);
-                
             }
-            // console.log(bgColors);
+            const option = value.split('-')
+            // console.log(option);
+            let thisClass = ''
+            const sizePix = option[1]?.indexOf('p')?(option[1]?.indexOf('v')?'px':`v${option[0]}`):'%'
+            const sizeValue = sizePix === 'px'?option[1]:option[1]?.slice(1)
 
-    }
-    
-    if(props.bg!==undefined){
-        // console.log(props.bg);
-        
-        if(Array.isArray(props.bg)){
-            
-            setBgOptions(props.bg)
-        }else if(props.bg[0] ==='$'){
-            styles.background = props.bg.slice(1)
-        }else if(new RegExp('^.*.(jpg|png|gif|webp|avif|svg)$').test(props.bg)){
-            // console.log(props.bg);
-            styles.backgroundImage = `url("${props.bg}")`
-        }
-        else{
-            const arr = props.bg.split(' ')
-            if(arr.length>1){
-                setBgOptions(arr)
-            }else{
-                className[`bg-${props.bg}`] = true
-                delete styles.background
+            switch(option[0]){
+                case 'w':size[0] = sizeValue + sizePix;break;
+                case 'h':size[1] = sizeValue + sizePix;break;
+                case 'c':className[`bc-${option?.[1]}`] = true;break;
+                case 's':className[`bs-${option?.[1]}`] = true;break;
+                case 'x':className[`bp-x-${option?.[1]}`] = true;break;
+                case 'y':className[`bp-y-${option?.[1]}`] = true;break;
+                case 'r':className[`bp-r-${option?.[1]}`] = true;break;
+                default:{
+                    for(let i of option){
+                        bgColors.push(i)
+                        if(bgColors.length>1){
+                            className[`bc-${bgColors[0]}`] = false
+                            className[`bc-${i}`] = false
+                            
+                            const lastColor = styles.backgroundColor?styles.backgroundColor:bgColors[0]
+                            
+                            styles.backgroundColor = `color-mix(in lch, ${lastColor}, ${i})`
+                        }
+                    }
+
+                    // thisClass = bgOptionActive[<keyof typeof bgOptionActive>option?.[0]]?.(option?.[1])
+                }break;
             }
- 
-        }
+            if(thisClass){
+                className[thisClass] = true
+            }
+
+            // console.log(size,styles);
+            if(size[0]!=='auto'||size[1]!=='auto'){
+                styles.backgroundSize = `${size[0]} ${size[1]}`
+            }
+            
+        })
     }
 
 
