855ce8c2fd7d27b826fe6dfec8628aa2fbd92c59
lilmonix3 <wwssjjhhaamm@qq.com>
2025-04-02 00:49:27 +0800

    chore：配置打包命令

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,35 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import {resolve} from 'path'
+const pathResolve = (dir: string) => resolve(__dirname, dir)
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': pathResolve('./src')  // 确保别名配置正确
+    }
+  },
+
   server:{
     port:8088
   },
-  build:{
-    rollupOptions:{
-      input:'index.ts'
-    }
+  build: {
+    lib: {
+      entry: pathResolve('./src/index.ts'),
+      name: 'noStyleUi',
+      fileName: (format) => `no-style-ui.${format}.js`
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    },
+    outDir: 'dist' // 指定输出目录
   },
 })
