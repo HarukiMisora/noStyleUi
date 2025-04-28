5ee26728d6caa6c6da1cff0222781da820758fbc
lilmonix3 <wwssjjhhaamm@qq.com>
2025-04-28 12:28:27 +0800

    fix：修复Group cusProps 类型推导错误

--- src/noStyleUi/interface/compoment.ts
+++ src/noStyleUi/interface/compoment.ts
@@ -5,7 +5,7 @@ export type buttonEffectType = 'box'|'text'|'none'|'biger'|'small'|'rotate'
 export type buttonType = 'default'|'primary'|'info'|'success'|'warning'|'error'|'tertiary'|'none'
 export type buttonAbstractType = 'default'|'unseen'|'dash'|'glass'|'round'
 export type buttonSizeType = 'mini'|'small'|'default'|'biger'
-export type groupCusPropsType = [string,string][]
+export type groupCusPropsType = [string,any][]
 import type {PropType} from 'vue'
 
 
